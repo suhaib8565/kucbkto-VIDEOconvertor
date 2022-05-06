19f9a19ec3db9369eaededc7fdcf7a17c9b3c9f4
suhaib8565 <91482773+suhaib8565@users.noreply.github.com>
2022-05-06 09:46:20 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -41,7 +41,7 @@ async def notice(event):
 async def source(event):
     await Drone.send_message(event.chat_id, source_text,
                     buttons=[[
-                         Button.url("GITHUB.", url="https://github.com/Vasusen-code/VideoConvertor/tree/public")]])
+                         Button.url("Youtube.", url="https://www.youtube.com/channel/UCsNkiEm2go3Zyzum7e3PpBg/about")]])
                                     
     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
